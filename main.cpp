c155b67f091c85971900508a2935a108f39171f2
yr <yr@love.com>
2026-02-01 17:08:04 +0800

    seventh

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main() {
         std::cout << "i = " << i << std::endl;
     }
     test();
+
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
