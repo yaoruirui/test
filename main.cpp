d6a8b0cd7756b57a51613a59c22692854fc78707
yr <yr@love.com>
2026-02-01 14:06:59 +0800

    fouth

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+void test() {
+    for (int i = 0; i <= 5; i++) {
+        std::cout << "hello world" << std::endl;
+        std::cout << "1" << std::endl;
+    }
+
+}
+
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
@@ -10,7 +18,8 @@ int main() {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         std::cout << "i = " << i << std::endl;
     }
-
+    test();
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
+
